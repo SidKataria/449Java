538ff8fc9a06c11a89a9be7ab25f322426fe80c4
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2019-01-22 19:47:36 -0700

    Add files via upload

--- Parsing.java
+++ Parsing.java
@@ -5,6 +5,7 @@
  * @version Can only get name of file and completely read through
  */
 import java.io.*;
+import java.util.Arrays;
 
 public class Parsing
 {
@@ -14,17 +15,26 @@ public class Parsing
      */
     public Parsing()
     {
-        
+        for (int i = 0; i<8; i++) {
+            fpAssignment[i] = null;
+            System.out.println(fpAssignment[i]);
+        }
     }
     //Final Arrays for checking input
     private final String[] Machines = {"1", "2", "3", "4", "5", "6", "7", "8"};
     private final String[] Tasks = {"A", "B", "C", "D", "E", "F", "G", "H"};
     
-    //File name
-    public static String inputName;
+    //Keyword Strings
+    public static String inputName, inputFPA;
+    
+    //Basic counter
+    public static int counter;
+    //Array for Forced Partial Assignments
+    //fpAssignment[i] = j; task j to machine i (*mach 1* = fpAssignment[0]) 
+    public static String[] fpAssignment = new String[8];
     
     //Checking for key words
-    public static boolean name;
+    public static boolean name, fpa;
     
     //Method for parsing
     public static void parsing(String filename) throws Exception{
@@ -46,16 +56,44 @@ public class Parsing
                 switch (foundWord) {
                 case "name:":
                     name = false;
-                    line = br.readLine();
-                    inputName = line.trim();
+                    inputName = br.readLine();
                     //Getting name of input file
-                    if (inputName != null) {
+                    inputName.trim();
+                    if (inputName.isEmpty() != true) {
                         name = true;
                         System.out.println("Input file name: " + inputName);
                     }
-                    else //Else not reaching, input name can still take null value
-                        System.out.println("No name for input File");
+                    else
+                        System.out.println("No name for input file");
                     break;
+                case "forced partial assignment:":
+                    fpa = false;
+                    counter = 0;
+                    while ((inputFPA = br.readLine()) != null) {
+                        inputFPA.trim();
+                        if (inputFPA.isEmpty())
+                            break;
+                        String[] givenIn = inputFPA.split("[(,)]");
+                        String givenMach = givenIn[1];
+                        String givenTask = givenIn[2];
+                        if (counter != 8) {
+                            if (fpAssignment[Integer.parseInt(givenMach) - 1] == null) {
+                                fpAssignment[Integer.parseInt(givenMach) - 1] = givenTask;
+                                System.out.println("Given mach: " + givenMach + " Given task: " + givenTask);
+                                fpa = true;
+                            }
+                            else {
+                                //Quit System
+                                System.out.println("Forced Partial Assignment error");
+                            }
+                        }
+                        else {
+                            //Quit System
+                            System.out.println("Forced Partial Assignment error");
+                        }
+                        counter++;
+                    }
+                    //To check what the final Array of Forced Partial Assignments: System.out.println(Arrays.toString(fpAssignment));
                 }
             }
         }catch (FileNotFoundException e) {
